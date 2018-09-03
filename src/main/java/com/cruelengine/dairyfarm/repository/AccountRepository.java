d7f1526c2e4b679a379ed70a8feb007d042465dc
CruelEngine <babhineethbhat@gmail.com>
2018-09-03 23:48:21 +0530

    reactive crud repo first query

--- src/main/java/com/cruelengine/dairyfarm/repository/AccountRepository.java
+++ src/main/java/com/cruelengine/dairyfarm/repository/AccountRepository.java
@@ -1,13 +1,17 @@
 package com.cruelengine.dairyfarm.repository;
 
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cruelengine.dairyfarm.models.Account;
 
+import reactor.core.publisher.Mono;
+
 @Repository
 public interface AccountRepository extends ReactiveCrudRepository<Account, String>{
 	
-	Account findByName();
+	@Query("{'username' : ?0}")
+	Mono<Account> findByName();
 	
 }
